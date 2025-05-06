126df6e448ea84218d28f00726fa191eee046de1
Fedor <fedor.shopping@gmail.com>
2025-05-07 06:54:09 +0900

    version 2.0

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Smart contracts for mini games Banzai GameFi ecosystem (EVM)
 Players send same amount stakes to smart contract, storing in pool. When pool is filled 
 with 10 stakes, after 2 blocks smart contract detects winner and sends him 90% of pool.
 
+v2.0 RELEASED
+
+Only oracle can call detectWinner() and he passes random number on every call. 
+
 Simple MEV/Frontrunning protection is implemented: winner detected after some delay, when
 transactions was already 2 blocks behind in chain. So bots cant try to play and after rewrite 
 their transaction if they losed.
--- contracts/GameA.sol
+++ contracts/GameA.sol
@@ -1,25 +1,23 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
+pragma solidity ^0.8.28;
 
-// pools:
-// A - 0.01
-// B - 0.05
-// C - 0.1
+// version 2.0
+// pools stakes: A - 0.01, B - 0.05, C - 0.1
 
-contract Game_neo_A {
-  uint256 public constant STAKE = 0.01 ether;
-  uint256 round = 1;
+contract Banzai_game_A {
+  uint256 constant STAKE = 0.01 ether;
+  uint256 public round = 1;
   uint256 lastBlock = 0;
   address public lastWinner;
   address constant CREATOR = 0x90fe1986092Ec963C4e9368837D02CB297f545Fe;
-  uint8 constant WINNERS_HISTORY_SIZE = 20;
+  address constant ORACLE = 0xe7c161b9CCc53447E57d322138111AaBC322Dece;
+  mapping(address => uint256) public stakes;
   address[] pool;
-  address[] winners;
 
   function claimReward() public payable {
     require(msg.sender == CREATOR, "Only creator can get rewards");
     require(pool.length < 1, "Pool must be empty");
-    (bool sentReward,) = CREATOR.call{ value: address(this).balance - 0.01 ether }("");
+    (bool sentReward,) = CREATOR.call{ value: address(this).balance - 0.005 ether }("");
     require(sentReward, "Failed to send reward");
   }
 
@@ -27,64 +25,38 @@ contract Game_neo_A {
     return pool;
   }
 
-  function showWinners() external view returns (address [] memory) {
-    return winners;
-  }
-
-  function showChance() public view returns (uint8) {
-    uint8 myChance = 0;
-    for (uint8 i = 0; i < pool.length; i++) {
-         if (pool[i] == msg.sender) {
-            myChance += 10;
-         }
-    }
-    return myChance;
-  }
-
   function detectWinner() public payable {
+      require(msg.sender == ORACLE, "Only oracle can call this method");
       require(pool.length > 9, "Pool is not full");
-      require(block.number > lastBlock+2, "Please wait 2 more blocks to detect winner");
+      require(block.number > lastBlock+2, "Please wait 2 more blocks");
       
-      uint256 diff = block.number - lastBlock;
-      uint rand1 = uint(keccak256(abi.encodePacked(block.timestamp, diff, msg.sender, round*5))) % 10;
-      uint rand2 = uint(keccak256(abi.encodePacked(block.number, diff*4, msg.sender, round*8))) % 10;
-      uint rand3 = uint(keccak256(abi.encodePacked(block.coinbase, diff*7, msg.sender, round*3))) % 10;
-
-      uint256 ind = uint(keccak256(abi.encodePacked(block.timestamp, pool[rand1], msg.sender, pool[rand2], round * diff, pool[rand3]))) % 10;
+      uint256 ind = msg.value % 10;
       address winner = pool[ind];
       lastWinner = winner;
-      delete pool; // reset pool
-      round++;
 
-      if (winners.length < WINNERS_HISTORY_SIZE) {
-        winners.push(winner);
-      } else {
-        for (uint256 i = 0; i < WINNERS_HISTORY_SIZE-1; i++) {
-            winners[i] = winners[i+1];
-        }
-        winners[WINNERS_HISTORY_SIZE-1] = winner;
+      for (uint256 i = 0; i < 10; ++i) {
+        address c = pool[i];
+        delete stakes[c];
       }
+
+      delete pool; // reset pool
+      ++round;
 
       (bool sent,) = winner.call{ value: STAKE*9 }("");
-
-      // every 5 rounds send rewards to developer
-      if (sent && round % 5 == 0) {
-        (bool sentReward,) = CREATOR.call{ value: address(this).balance - 0.015 ether }("");
-        require(sentReward, "Failed to send reward to devs");
-      }
+      require(sent, "Failed to send prize");
   }
 
   function play() public payable {
+    require(tx.origin == msg.sender, "No stakes from smart contracts!");
     require(msg.value == STAKE, "Invalid amount!");
+    require(pool.length < 10, "Pool is filled, please wait");
 
-    if (pool.length > 9) {
-        detectWinner();
+    if (stakes[msg.sender] > 0) {
+        require(stakes[msg.sender] < 5, "Max 5 stakes per round");
     }
 
-    uint8 myChance = showChance();
-    require(myChance < 50, "Stake limit, not more than 5 stakes per round from 1 address");
-
     pool.push(msg.sender);
+    stakes[msg.sender]++;
     lastBlock = block.number;   
   }
 
